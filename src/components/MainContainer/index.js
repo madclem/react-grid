e4444d65d85ef9dd32536bceda5073fbb54a1020
clementchenebault <clem.chenebault@gmail.com>
2019-01-05 00:57:30 +0100

    grid template areas

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -5,31 +5,43 @@ export default function MainContainer(props) {
     const MainContainerStyle = styled.div`
         justify-content: center;	
         display: grid;
-        grid-template-columns: [col1-start] 100px [col2-start] 100px [col3-start] 100px [col3-end];
+        grid-template-columns: [col1-start] 200px [col2-start] 200px [col3-start] 200px [col3-end];
         grid-template-rows: auto;
         grid-column-gap: 10px;
         grid-row-gap: 10px;
+        grid-template-areas:
+            "header header header"
+            "sidebar content content"
+            "footer footer footer"
     `;
     const Box = styled.div`
         background-color: rgb(120,70,123);
         border: 5px solid rgb(88,55,112);
-        // height: 100px;
 		color: #fff;
 		border-radius: 5px;
 		padding: 20px;
 		font: 150%/1.3 Lucida Grande,Lucida Sans Unicode,Lucida Sans,Geneva,Verdana,sans-serif;
-        grid-column: ${props => (props.cs || 'auto' ) + ' / ' + (props.ce || 'auto') };
-        grid-row: ${props => (props.rs || 'auto' ) + '/ span ' + (props.rspan || 1) };
-        // grid-column: ${props => (props.cs || 'auto' ) + '/ span ' + (props.cspan || 1) };
-        // grid-row: ${props => (props.rs || 'auto' ) + '/ span ' + (props.rspan || 1) };
+    `;
+
+    const Header = styled(Box)`
+        grid-area: header;
+    `;
+    const Sidebar = styled(Box)`
+        grid-area: sidebar;
+    `;
+    const Content = styled(Box)`
+        grid-area: content;
+    `;
+    const Footer = styled(Box)`
+        grid-area: footer;
     `;
 
     return (
         <MainContainerStyle>
-            <Box cs={1} ce={'col3-start'}> A </Box>
-            <Box cs={'col3-start'} ce={'col3-end'} rspan={2}> B </Box>
-            <Box cs={1} rs={2}> C </Box>
-            <Box cs={2} rs={2}> D </Box>
+            <Header> Header </Header>
+            <Sidebar> Sidebar </Sidebar>
+            <Content> Content </Content>
+            <Footer> Footer </Footer>
         </MainContainerStyle>
     );
 }
