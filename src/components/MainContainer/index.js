554069859113b4d2f830771d2921024a4400dda5
clementchenebault <clem.chenebault@gmail.com>
2019-01-05 00:48:04 +0100

    use named tracks

--- src/components/MainContainer/index.js
+++ src/components/MainContainer/index.js
@@ -5,7 +5,7 @@ export default function MainContainer(props) {
     const MainContainerStyle = styled.div`
         justify-content: center;	
         display: grid;
-        grid-template-columns: 100px 100px 100px;
+        grid-template-columns: [col1-start] 100px [col2-start] 100px [col3-start] 100px [col3-end];
         grid-template-rows: auto;
         grid-column-gap: 10px;
         grid-row-gap: 10px;
@@ -18,14 +18,16 @@ export default function MainContainer(props) {
 		border-radius: 5px;
 		padding: 20px;
 		font: 150%/1.3 Lucida Grande,Lucida Sans Unicode,Lucida Sans,Geneva,Verdana,sans-serif;
-        grid-column: ${props => (props.cs || 'auto' ) + '/ span ' + (props.cspan || 1) };
+        grid-column: ${props => (props.cs || 'auto' ) + ' / ' + (props.ce || 'auto') };
         grid-row: ${props => (props.rs || 'auto' ) + '/ span ' + (props.rspan || 1) };
+        // grid-column: ${props => (props.cs || 'auto' ) + '/ span ' + (props.cspan || 1) };
+        // grid-row: ${props => (props.rs || 'auto' ) + '/ span ' + (props.rspan || 1) };
     `;
 
     return (
         <MainContainerStyle>
-            <Box cs={1} ce={3} cspan={2}> A </Box>
-            <Box cs={3} ce={4} rspan={2}> B </Box>
+            <Box cs={1} ce={'col3-start'}> A </Box>
+            <Box cs={'col3-start'} ce={'col3-end'} rspan={2}> B </Box>
             <Box cs={1} rs={2}> C </Box>
             <Box cs={2} rs={2}> D </Box>
         </MainContainerStyle>
